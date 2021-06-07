148eb9c097b3e95758b3d5417bba4666e1073efa
Maryam Tahir <maryamtah8@gmail.com>
2021-06-06 21:36:13 -0400

    adding components

---
+++ src/Components/About/About.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const About = props => {
+    return (
+      <section id="about">
+        <div className="wrapper">
+          <article>
+            <div className="title">
+              <h3>A little about myself</h3>
+              <p className="separator" />
+            </div>
+            <div className="desc full">
+            <p>
+              I am a web developer
+              </p>
+            <p>
+              I really enjoy solving problems as well as making things pretty and easy to use. I
+              can't stop learning new things; the more, the better.
+              </p>
+          </div>
+          <div className="title">
+            <h3>What does she do?</h3>
+            <p className="separator" />
+          </div>
+          <div className="desc">
+            <h4 className="subtitle">I'm a programmer.</h4>
+            <p>
+              For the front-end I usually work with HTML, CSS, and, Javascript, including
+              popular frameworks like ReactJS.
+            </p>
+            <p>
+              For the back-end I also work with Javascript (NodeJS, Express, MongoDB, etc). But, of
+              course, whenever the project requires PHP, I do PHP as well (Wordpress, Laravel, etc).
+            </p>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default About;
